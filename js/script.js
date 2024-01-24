5119580ba79b8b89d14d9009d0b4c69e459cc426
Hajar Lamharzi <Hajarlamharzi@gmail.com>
2024-01-25 00:13:32 +0100

    Portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*===========typing animation=======*/
+var typed = new  Typed(".typing", {
+    strings:["","Web Design","Web Developer","Graphic Design","Develpement digital"],
+    typeSpeed:100,
+    BackSpeed:100,
+    loop:true
+})
